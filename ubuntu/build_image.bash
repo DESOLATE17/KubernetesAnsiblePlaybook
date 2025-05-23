120a9fd16ac59a97456f1358408633849f8cd0c0
DESOLATE17 <taktashovadasha@yandex.ru>
2025-05-23 13:52:21 +0300

    refactoring

--- ubuntu/build_image.bash
+++ ubuntu/build_image.bash
@@ -1,4 +1,4 @@
-# PASSWORD=$(openssl rand -base64 10 | tr -d '/+=' | cut -c1-10)
+PASSWORD=$(openssl rand -base64 10 | tr -d '/+=' | cut -c1-10)
 PASSWORD=$2
 USERNAME=$1
 
@@ -7,5 +7,11 @@ docker tag ubuntu-ssh:latest custom/ubuntu-ssh:latest
 
 docker save custom/ubuntu-ssh:latest -o custom-ubuntu.tar
 sudo k3s ctr images import custom-ubuntu.tar
+
+scp custom-ubuntu.tar iu5-s-1-1@iu5-s-1-1:/home/iu5-s-1-1/
+ssh -t iu5-s-1-1@iu5-s-1-1 sudo k3s ctr images import custom-ubuntu.tar
+
+scp custom-ubuntu.tar iu5-s-1-2@iu5-s-1-2:/home/iu5-s-1-2/
+ssh -t iu5-s-1-2@iu5-s-1-2 sudo k3s ctr images import custom-ubuntu.tar
 
 # psql -h <postgres_host> -U <postgres_user> -d <postgres_db> -c "INSERT INTO users (username, password) VALUES ('$USERNAME', '$PASSWORD');"
--- ubuntu/create_user.sh
+++ ubuntu/create_user.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <username>"
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <username> [password]"
   exit 1
 fi
 
 USERNAME=$1
+PASSWORD=${2:-$(openssl rand -base64 12)}  # Generate random password if not provided
 
+# Check if user already exists
 if id "$USERNAME" &>/dev/null; then
   echo "User $USERNAME already exists."
   exit 0
 fi
 
-PASSWORD=$2
+# Create user with home directory and bash shell
+useradd -m -s /bin/bash "$USERNAME" || {
+  echo "Failed to create user $USERNAME"
+  exit 1
+}
 
-useradd -m -s /bin/bash $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
+# Set password
+echo "$USERNAME:$PASSWORD" | chpasswd || {
+  echo "Failed to set password for $USERNAME"
+  exit 1
+}
 
-# Add user to sudo group (optional)
-usermod -aG sudo $USERNAME
+# Add to sudo group
+usermod -aG sudo "$USERNAME" && echo "Added $USERNAME to sudo group"
 
-# Log the username and password
-# echo "User created: $USERNAME"
-# echo "Password: $PASSWORD"
+echo "User $USERNAME created successfully"
+echo "Password: $PASSWORD"
